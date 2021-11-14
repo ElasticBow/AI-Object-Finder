9cc82278265aac74f89b5b15e62a33ac58f5988f
ElasticBow <79611589+ElasticBow@users.noreply.github.com>
2021-11-13 19:34:44 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
+objects = [];
 status = "";
 
 function setup() {
     canvas = createCanvas(480, 380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
 }
 
 function draw() {
     image(video, 0, 0, 480, 380);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = objects + "Found";
+            document.getElementById("objects").innerHTML = "Number of objects detected are : "+ objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function start()
@@ -19,4 +38,12 @@ function start()
 function modelLoaded() {
     console.log("Model Loaded!")
     status = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
